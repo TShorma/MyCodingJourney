ecc47bc7c6e41e54de044f91bc9a98ffa47b6ede
TShorma <94042467+TShorma@users.noreply.github.com>
2021-12-10 14:38:14 -0500

    Delete w2hw1bonus.js

--- w2hw1bonus.js
+++
@@ -1,4 +0,0 @@
-let prompt = require("prompt-sync")();
-let userString = prompt("Enter a string here: ")
-let userUpperString = userString.charAt(0).toUpperCase() + userString.slice(1);
-console.log(userUpperString);
